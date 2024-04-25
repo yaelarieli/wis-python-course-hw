12836b86576c22ae70b5fc8d657c7897ab2b7432
Yael Arieli <yaelarieli542@gmail.com>
2024-04-25 11:47:00 +0300

    change hw2

--- day02/hw2_guess_number.py
+++ day02/hw2_guess_number.py
@@ -3,20 +3,21 @@
 import random
 
 computer_think = random.randrange(1,21)
-user_guress=input("The computer think about a whole number betwee 1 and 20, guess what number: ")
 number_of_guess=0
 
+user_guress=int(input("The computer think about a whole number betwee 1 and 20, guess what number: "))
+
 while True:
-    if int(user_guress)==int(computer_think):
+    if user_guress==computer_think:
         print("You are correct!")
-        number_of_guess=number_of_guess+1
+        number_of_guess+= 1
         break
-    elif int(user_guress) > int(computer_think):
-        number_of_guess=number_of_guess+1
+    elif user_guress > computer_think:
+        number_of_guess+= 1
         print("Your number is bigger then the computer number, guess again:")
-        user_guress=input()
-    elif int(user_guress) < int(computer_think):
-        number_of_guess=number_of_guess+1
+    elif user_guress < computer_think:
+        number_of_guess+= 1
         print("Your number is smaller then the computer number, guess again:")
-        user_guress=input()
+
+    user_guress=int(input())
 print(f"You had {number_of_guess} guesses")
