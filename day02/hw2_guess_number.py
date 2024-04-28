daa80c11bda7182d7bae13d5b5160cc60fb903fd
Yael Arieli <yaelarieli542@gmail.com>
2024-04-28 16:00:18 +0300

    change hw2

--- day02/hw2_guess_number.py
+++ day02/hw2_guess_number.py
@@ -8,15 +8,13 @@ number_of_guess=0
 user_guress=int(input("The computer think about a whole number betwee 1 and 20, guess what number: "))
 
 while True:
+    number_of_guess += 1
     if user_guress==computer_think:
         print("You are correct!")
-        number_of_guess+= 1
         break
     elif user_guress > computer_think:
-        number_of_guess+= 1
         print("Your number is bigger then the computer number, guess again:")
     elif user_guress < computer_think:
-        number_of_guess+= 1
         print("Your number is smaller then the computer number, guess again:")
 
     user_guress=int(input())
